24f42c1cafaf1ee3104e9219c6f1790bec4c4bf4
Cathy V. Amamangpang <159250691+cathyang2@users.noreply.github.com>
2024-12-11 01:28:00 -0800

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -21,3 +21,18 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (!cacheWhitelist.includes(cacheName)) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
